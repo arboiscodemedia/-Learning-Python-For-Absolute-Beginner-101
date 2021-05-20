7c98a5e4ca40ccc0fca3f43e4aa91697516b4b52
Ricardo Arbois <83438953+arboiscodemedia@users.noreply.github.com>
2021-05-21 07:24:21 +0800

    Add files via upload

---
+++ guess_number.py
@@ -0,0 +1,16 @@
+import random
+
+num = random.randint(1,10)
+
+guess = None
+
+while guess != num:
+    guess = input("guess a number between 1 and 10:")
+    guess = int(guess)
+
+    if guess == num:
+        print("Congratulations! you won!")
+        break
+    else:
+        print("nope, sorry. try again!")
+        
